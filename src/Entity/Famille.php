c2a13aced46ff35320c7717565c99823c4fef5be
Littlebridwarrior <camille.dorland@gmail.com>
2023-05-28 17:29:01 +0200

    Entity Famille

---
+++ src/Entity/Famille.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FamilleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FamilleRepository::class)]
+class Famille
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
