eb83eb48de7bddb4a0f89dd8d30df87b142c090e
Littlebridwarrior <camille.dorland@gmail.com>
2023-05-28 23:08:03 +0200

    Ajout Entity Niveau

--- src/Entity/Niveau.php
+++ src/Entity/Niveau.php
@@ -13,8 +13,23 @@ class Niveau
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 25)]
+    private ?string $intitule = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
     }
 }
