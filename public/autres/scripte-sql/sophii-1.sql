35dcc8d7bf87a9841b00546b5a9b032b50e3f0ad
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-06-07 22:31:35 +0200

    commit pour utilisation mac

--- public/autres/scripte-sql/sophii-1.sql
+++ public/autres/scripte-sql/sophii-1.sql
@@ -14,10 +14,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 -- Listage de la structure de la base pour sophii
 DROP DATABASE IF EXISTS `sophii`;
-CREATE DATABASE IF NOT EXISTS `sophii` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `sophii` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sophii`;
 
 -- Listage de la structure de table sophii. activite
