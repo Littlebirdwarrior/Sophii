67a243d2bf1d92644322150aa60516e2abc829f2
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-06-09 00:25:03 +0200

    show classe a continuer

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Classe;
+use App\Repository\ClasseRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +18,19 @@ class ClasseController extends AbstractController
         
         return $this->render('classe/index.html.twig', [
             'classes' => $classes
+        ]);
+    }
+
+    //*details
+    #[Route('/intern/{id}', name: 'show_classe')]
+    public function show( ClasseRepository $classeRepository, Classe $classe): Response 
+    {
+        $classe_id = $classe->getId();
+
+        $sessionsLeft = $classeRepository->getSessionLeft($classe_id);
+
+        return $this->render('classe/show.html.twig', [
+            'classe' => $classe
         ]);
     }
 }
