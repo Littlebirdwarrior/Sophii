da23dae33705363324a14a8e57a0d3f89b9252bb
Littlebirdwarrior <camille.dorland@gmail.com>
2023-07-17 14:15:20 +0200

    Creation vue Activite

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Activite;
+use App\Repository\ActiviteRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ActiviteController extends AbstractController
 {
     #[Route('/activite', name: 'app_activite')]
-    public function index(): Response
+    public function index( ManagerRegistry $doctrine): Response
     {
+        $activites = $doctrine->getRepository( Activite::class)->findBy([], ["titre" => "ASC"]);
+
         return $this->render('activite/index.html.twig', [
-            'controller_name' => 'ActiviteController',
+            'activites' => 'activites',
         ]);
     }
+
+//*details
+    #[Route('/activite/{id}', name: 'show_activite')]
+    public function show( ActiviteRepository $activiteRepository, Activite $activite): Response
+    {
+        $activite_id = $activite->getId();
+
+        return $this->render('enseignant/show.html.twig', [
+            'activite' => $activite
+        ]);
+    }
+
+
 }
