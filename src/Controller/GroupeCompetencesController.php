f8e85bf6a22541888db73157d1fcf760916ef867
Littlebirdwarrior <camille.dorland@gmail.com>
2023-07-17 14:51:22 +0200

    ajout groupe competences controller

---
+++ src/Controller/GroupeCompetencesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GroupeCompetencesController extends AbstractController
+{
+    #[Route('/groupe/competences', name: 'app_groupe_competences')]
+    public function index(): Response
+    {
+        return $this->render('groupe_competences/index.html.twig', [
+            'controller_name' => 'GroupeCompetencesController',
+        ]);
+    }
+}
