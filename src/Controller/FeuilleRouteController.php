5bc8b05451fb30f076e57023d972962bf5b6edcf
Littlebirdwarrior <camille.dorland@gmail.com>
2023-08-01 15:19:36 +0200

    détail feuille route ok

--- src/Controller/FeuilleRouteController.php
+++ src/Controller/FeuilleRouteController.php
@@ -2,17 +2,78 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\FeuilleRoute;
+use App\Repository\FeuilleRouteRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FeuilleRouteController extends AbstractController
 {
-    #[Route('/feuille/route', name: 'app_feuille_route')]
-    public function index(): Response
+    #[Route('/feuille_route', name: 'app_feuille_route')]
+    public function index( ManagerRegistry $doctrine ): Response
     {
+        $feuille_routes = $doctrine->getRepository( FeuilleRoute::class)->findBy([]);
+
         return $this->render('feuille_route/index.html.twig', [
-            'controller_name' => 'FeuilleRouteController',
+            'feuille_routes' => $feuille_routes,
+        ]);
+    }
+
+    #[Route('/feuille_route/add', 'feuille_route.add', methods: ['GET', 'POST'])]
+    #[Route('/feuille_route/{id}/update', name: 'update_feuille_route')]
+
+    public function add(ManagerRegistry $doctrine, FeuilleRoute $feuille_route = null, Request $request) : Response
+    {
+        if(!$feuille_route){
+            $feuille_route = New FeuilleRoute();
+        }
+
+        $form = $this->createForm(FeuilleRoute::class, $feuille_route);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $feuille_route = $form->getData();
+            $entityManager = $doctrine->getManager();
+            //(prepare selon PDO)
+            $entityManager->persist($feuille_route);
+            //insert into (execute selon PDO)
+            $entityManager->flush();
+
+            //redirection vers la route des enseignants
+            return $this->redirectToRoute('app_feuille_route');
+        }
+
+        //redirection vers la vue du Form
+        return $this->render('feuille_route/add.html.twig', [
+            'formAddEnseignant' => $form->createView(),
+            'update'=> $feuille_route->getId()
+        ]);
+
+    }
+
+    #[Route('/feuille_route/{id}/delete', name: 'delete_feuille_route')]
+    public function delete( ManagerRegistry $doctrine, FeuilleRoute $feuille_route):Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $entityManager->remove($feuille_route);
+        //persist pas utile, flush, execute requete
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_feuille_route');
+    }
+
+    //*details
+    #[Route('/feuille_route/{id}', name: 'show_feuille_route')]
+    public function show( FeuilleRouteRepository $FeuilleRouteRepository, FeuilleRoute $feuille_route): Response
+    {
+        $feuille_route_id = $feuille_route->getId();
+
+        return $this->render('feuille_route/show.html.twig', [
+            'feuille_route' => $feuille_route
         ]);
     }
 }
--- src/Entity/FeuilleRoute.php
+++ src/Entity/FeuilleRoute.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use IntlDateFormatter;
 
 #[ORM\Entity(repositoryClass: FeuilleRouteRepository::class)]
 class FeuilleRoute
@@ -70,6 +71,27 @@ class FeuilleRoute
         return $this;
     }
 
+    public function getDates()
+    {
+        date_default_timezone_set('Europe/Paris');
+        //je recupére mes objet datetime
+        $dateDebut =  $this->dateDebut;
+        $dateFin =  $this->dateFin;
+
+        //je formate mes objetS
+
+
+        date_default_timezone_set('Europe/Moscow');//Moscou = GMT+3
+
+        $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::NONE, IntlDateFormatter::NONE);
+        $formatter->setPattern('EEEE, d MMMM');
+
+        $dateDebutString = $formatter->format($dateDebut);
+        $dateFinString = $formatter->format($dateFin);
+
+        return $dateDebutString . " - " . $dateFinString;
+    }
+
     public function getSemaine(): ?int
     {
         return $this->semaine;
@@ -92,6 +114,17 @@ class FeuilleRoute
         $this->validation = $validation;
 
         return $this;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getValidation(): ?string
+    {
+        if ($this->validation === true){
+            return 'oui';
+        }
+        return 'non';
     }
 
     public function getEleve(): ?Eleve
@@ -131,6 +164,10 @@ class FeuilleRoute
         }
 
         return $this;
+    }
+
+    public function __toString(){
+        return "feuille de route de " . $this->getEleve();
     }
 
 }
