9d0fabc85004348ef8347035e1833841721b8d72
Littlebirdwarrior <camille.dorland@gmail.com>
2023-09-14 16:20:13 +0200

    ajout du js dans pour le collectionType

--- src/Form/BulletinGroupeCompetencesType.php
+++ src/Form/BulletinGroupeCompetencesType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\BulletinGroupeCompetences;
+use App\Entity\GroupeCompetences;
+use App\Entity\Trimestre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +15,19 @@ class BulletinGroupeCompetencesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('acquisition')
-            ->add('bulletin')
-            ->add('groupe_competence')
+            //->add('acquisition')
+
+            ->add('groupe_competence', EntityType::class, [
+                // looks for choices from this entity
+                'class' => GroupeCompetences::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'titre',
+
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                // 'expanded' => true,
+            ])
         ;
     }
 
