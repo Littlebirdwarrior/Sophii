939e2648dcfbbcb0bbe096615be3284ec32311fd
Littlebirdwarrior <camille.dorland@gmail.com>
2023-10-04 14:27:44 +0200

    Ajout d'un honypot sur le formulaire de login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,15 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 /*Pas de logique dans ce controller, contient deux méthodes, login et logout,
 la logique est contenue dans le composant App Authenticator*/
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
          if ($this->getUser()) {
             return $this->redirectToRoute('');
@@ -25,6 +27,14 @@ class SecurityController extends AbstractController
 
         if($error) {
             $this->addFlash('error', 'Vos identifiants ne sont pas valide');
+        }
+
+        // Vérifiez le champ honeypot
+        $honeypot = $request->request->get('honeypot');
+
+        if (!empty($honeypot)) {
+            // Vous pouvez prendre des mesures ici, par exemple, jeter une exception
+            throw new CustomUserMessageAuthenticationException('Bot détecté !');
         }
 
         return $this->render('security/login.html.twig', [
